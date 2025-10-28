403420fa9a43efb3bd558f839c7f7c70bd80e3ab
Haigh <ryan.haigh@stfc.ac.uk>
2025-10-28 11:23:25 +0000

    Make Neopixel lights change colour like a wave
    - creating function update_multiple_pixels to allow you to change any
    pixels at the same time
    - create rainbow_wave function that send requests to update function
      telling it to update all pixels with the wave colour

--- code.py
+++ code.py
@@ -8,6 +8,7 @@ import board
 import neopixel
 
 
+
 strip = neopixel.NeoPixel(board.GP16, 30, brightness=0.05)
 rgb = [255, 0, 0]
 DEBUG_PRINT = True
@@ -73,7 +74,63 @@ def rainbow_cycle(delay: float = 0.002) -> bool:
         return True
 
 
+def update_multiple_pixels(pixels, updates):
+    for index, color in updates.items():
+        time.sleep(0.05)
+        pixels[index] = color
+        print(color)
+
+
+def rainbow_wave(delay: float = 0.002) -> None:
+
+    color_sequence = [
+        (255, 0, 0),  # Red
+        (255, 255, 0),  # Yellow
+        (0, 255, 0),  # Green
+        (0, 255, 255),  # Cyan
+        (0, 0, 255),  # Blue
+        (255, 0, 255),  # Magenta
+    ]
+
+
+    def running_function(
+        start_colour: Tuple,
+        end_colour: Tuple,
+        num_pixels: int,
+    ) -> None:
+        update_dict = {
+            i: (
+                int(
+                    start_colour[0]
+                    + (end_colour[0] - start_colour[0]) * i / (num_pixels - 1)
+                ),
+                int(
+                    start_colour[1]
+                    + (end_colour[1] - start_colour[1]) * i / (num_pixels - 1)
+                ),
+                int(
+                    start_colour[2]
+                    + (end_colour[2] - start_colour[2]) * i / (num_pixels - 1)
+                ),
+            )
+            for i in range(num_pixels)
+        }
+
+        update_multiple_pixels(strip, update_dict)
+
+
+
+    for i in range(len(color_sequence) - 1):
+        running_function(color_sequence[i], color_sequence[i+1], len(strip))
+        time.sleep(delay)
+
+
 while True:
+    debug_print("Turning pixels black")
+    strip.fill((0, 0, 0))
+    time.sleep(1)
+
     debug_print("BEGINNING OF MAIN WHILE LOOP")
-    if not rainbow_cycle():
-        break
+    rainbow_wave(0.001)
+    rainbow_cycle()
+    time.sleep(1)
